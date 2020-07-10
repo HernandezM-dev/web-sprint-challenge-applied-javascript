76db81aff41e9159732b8bb4f7bbcff0a91ebc0e
HernandezM-dev <HernandezM.dev@gmail.com>
2020-07-10 13:31:43 -0400

    working on cards

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,68 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+function createArticle(data){
+ 
+    const card = document.createElement('div'),
+          headline = document.createElement('div'),
+          author = document.createElement('div'),
+          imgContainer = document.createElement('div'),
+          img = document.createElement('img');
+          name = document.createElement('span');
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+    img.src = data.authorPhoto;
+
+    
+    headline.textContent = data.headline;
+    name.innerHTML = `by ${data.authorName}`;
+
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    // card.appendChild(name);
+    
+
+    card.addEventListener('click', event=>{
+        console.log(headline.innerHTML)
+    })
+    return card;
+}
+
+const cardContainer = document.querySelector('.cards-container');
+const articles = 'https://lambda-times-backend.herokuapp.com/articles'
+axios.get(articles)
+.then(function(res){
+    let selectArticles = []
+    Object.entries(res.data.articles).forEach((entry) =>{
+        selectArticles.push(entry)
+    })
+
+    let innerArticles = []
+    selectArticles.forEach(section =>{
+        console.log(section);
+        section.forEach(articles =>{
+            // console.log(articles.length);
+            // articles.forEach(article =>{
+            //     console.log(article)
+            // })
+            // const newArticle = createArticle(articles);
+            // console.log(newArticle)
+            // cardContainer.appendChild(newArticle);
+        })
+    })
+    
+    
+    
+
+})
+.catch(function(error){
+    debugger
+    console.log(error)
+})
